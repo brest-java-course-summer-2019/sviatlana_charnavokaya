7322095458fbc68d7cd178af1f61525baba69bf4
sviatlana-ch <sviatlana_charnavokaya@mail.ru>
2019-06-26 10:09:52 +0300

    add html tamplate and update delivery

--- delivery_cost/src/com/epam/brest2019/courses/Main.java
+++ delivery_cost/src/com/epam/brest2019/courses/Main.java
@@ -12,38 +12,45 @@ public class Main {
 
         BigDecimal weight;
         BigDecimal distance;
+        BigDecimal pricePerKg;
+        BigDecimal pricePerKm;
 
-        BigDecimal pricePerKg = new BigDecimal("30");
-        BigDecimal pricePerKm = new BigDecimal("50");
+        distance = readDataFromConsole("Enter the distance in kilometers or 'q' for quit");
+        weight = readDataFromConsole("Enter the weigth in kilograms or 'q' for quit");
+        pricePerKg = readPricePerKmFromProperties(distance);
+        pricePerKm = readPricePerKgFromProperties(weight);
+
+        System.out.println("weight = " + weight);
+        System.out.println("distance = " + distance);
+        System.out.println("pricePerKg = " + pricePerKg);
+        System.out.println("pricePerKm = " + pricePerKm);
+
+        BigDecimal price = weight.multiply(pricePerKg).add(distance.multiply(pricePerKm));
+        System.out.println("Price = " + price);
+
+
+    }
+
+    public static BigDecimal readDataFromConsole(String massage) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the weigth in kilograms or 'q' for quit");
-        String inputString = scanner.nextLine();
+        System.out.println(massage);
+        String inputString;
 
-        if (!inputString.toLowerCase().equals("q")) {
-            try {
-                weight = new BigDecimal(inputString);
-            } catch (NumberFormatException ex) {
-                System.out.println("Wrong value BigDecimal");
+        while (!scanner.hasNextBigDecimal()){
+            inputString = scanner.nextLine();
+            if (inputString.toLowerCase().equals("q")) {
+                System.out.println("Bye!");
+                return null;
             }
-        } else {
-            System.out.println("Bye!");
-            return;
-        }
+            System.out.println("Incorrect data. " + massage);
 
-        System.out.println("Enter the distance in kilometers or 'q' for quit");
+        }
         inputString = scanner.nextLine();
-        if (!inputString.toLowerCase().equals("q")) {
-            try {
-                distance = new BigDecimal(inputString);
-            } catch (NumberFormatException ex) {
-                System.out.println("Wrong value BigDecimal");
-            }
+        return new BigDecimal(inputString);
+    }
 
-        } else {
-            System.out.println("Bye!");
-            return;
-        }
+    public static BigDecimal readPricePerKmFromProperties(BigDecimal distance){
 
         FileInputStream fis;
         Properties properties = new Properties();
@@ -52,22 +59,45 @@ public class Main {
             fis = new FileInputStream("delivery_cost/resources/price_per_km.properties");
             properties.load(fis);
 
-            String dist1 = properties.getProperty("less10");
-            String dist2 = properties.getProperty("less100");
-            String dist3 = properties.getProperty("less500");
-            String dist4 = properties.getProperty("less1000");
-
         } catch (IOException e) {
-            System.err.println("ОШИБКА: Файл свойств отсуствует!");
+            System.err.println("Error: The file does not exist!");
         }
 
+        if (distance.doubleValue() <  10){
+            return new BigDecimal(properties.getProperty("less10"));
+        }else if (distance.doubleValue() >=  10 && distance.doubleValue() < 100) {
+                return new BigDecimal(properties.getProperty("less100"));
+        }else if (distance.doubleValue() >=  100 && distance.doubleValue() < 500) {
+                return new BigDecimal(properties.getProperty("less500"));
+        }else if (distance.doubleValue() >=  500 && distance.doubleValue() < 1000) {
+                return new BigDecimal(properties.getProperty("less1000"));
+        }else return new BigDecimal(properties.getProperty("less1000"));
+
+    }
 
 
-        System.out.println("weight = " + weight);
-        System.out.println("distance = " + distance);
+    public static BigDecimal readPricePerKgFromProperties(BigDecimal weight){
 
-        BigDecimal price = weight.multiply(pricePerKg).add(distance.multiply(pricePerKm));
-        System.out.println("Price = " + price);
+        FileInputStream fis;
+        Properties properties = new Properties();
+
+        try {
+            fis = new FileInputStream("delivery_cost/resources/price_per_kg.properties");
+            properties.load(fis);
+
+        } catch (IOException e) {
+            System.err.println("Error: The file does not exist!");
+        }
+
+        if (weight.doubleValue() <  100){
+            return new BigDecimal(properties.getProperty("less100"));
+        }else if (weight.doubleValue() >=  100 && weight.doubleValue() < 500) {
+            return new BigDecimal(properties.getProperty("less500"));
+        }else if (weight.doubleValue() >=  500 && weight.doubleValue() < 1500) {
+            return new BigDecimal(properties.getProperty("less1500"));
+        }else if (weight.doubleValue() >=  1500 && weight.doubleValue() < 2000) {
+            return new BigDecimal(properties.getProperty("less2000"));
+        }else return new BigDecimal(properties.getProperty("more2000"));
 
     }
 }
--- delivery_cost/src/com/epam/brest2019/courses/Main1111.java
+++ delivery_cost/src/com/epam/brest2019/courses/Main1111.java
@@ -4,32 +4,35 @@ import jdk.swing.interop.SwingInterOpUtils;
 
 public class Main1111 {
 
-   /* Main main = new Main();
-    System.out.println(main.getString());
-    System.out.println(main.calc(12f, 13d));*/
+   public static void main(String[] args) {
+       Main1111 main = new Main1111();
+       System.out.println(main.getString());
+       System.out.println(main.calc(12f, 13d));
 
-   /*public static void main(String[] args) {
 
-        System.out.println("Hi");
+
+       System.out.println("Hi");
         Integer newInteger = 12;
 
         // TODO: next
 
         System.out.println(newInteger);
+
+       double f = 0.0;
+       for (int i = 1; i <= 10; i++){
+           f += 0.1;
+       }
+       System.out.println("result" + f);
     }
 
-    puclic String getString(){
+    public String getString(){
         return "TEST";
     }
 
-    puclic Double calc(Float f, Double d){
+    public Double calc(Float f, Double d){
         return d + f;
     }
 
-    double f = 0.0;
-    for (int i = 1; i <= 10; i++){
-        f += 0.1;
-    }
-    System.out.println("result" + f);*/
+
 
 }
