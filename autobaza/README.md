ec6bb74d321bd635889e53b501f4c4d8b5cd8465
sviatlana-ch <sviatlana_charnavokaya@mail.ru>
2019-07-10 17:01:11 +0300

    init maven add module model

---
+++ autobaza/README.md
@@ -0,0 +1,1 @@
+# Autobaza Application
---
+++ autobaza/model/src/test/java/com/epam/brest/summer/courses2019/model/CarTest.java
@@ -0,0 +1,32 @@
+package com.epam.brest.summer.courses2019.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class CarTest
+{
+    Car car = new Car();
+
+  /*  @Test
+    public void getCarId() {
+        car.setCarId(15);
+        Assert.assertTrue(getCarId().equals(15));
+    }
+
+    @Test
+    public void getCarModel() {
+        car.setCarModel("Mersedes");
+        Assert.assertTrue(car.getCarModel().equals("Mersedes"));
+    }
+
+    @Test
+    public void getFixed() {
+        car.setFixed(true);
+        Assert.assertTrue(car.getFixed().equals(true));
+    }
+*/
+}
