924744cad22de0ad36443c35c678192512725d19
sviatlana-ch <sviatlana_charnavokaya@mail.ru>
2019-09-25 23:28:03 +0300

    change carresttest

--- autobaza/rest-app/src/main/java/com/epam/brest/summer/courses2019/rest_app/CarRestController.java
+++ autobaza/rest-app/src/main/java/com/epam/brest/summer/courses2019/rest_app/CarRestController.java
@@ -5,8 +5,9 @@ import com.epam.brest.summer.courses2019.service.CarService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -22,5 +23,39 @@ public class CarRestController {
     public Collection<Car> findAll() {
         LOGGER.debug("get all cars");
         return service.findAll();
+    }
+
+   /* @GetMapping(value = "/cars/&&&&&&&&&")
+    public List<CarStub> findAllStubs() {
+        LOGGER.debug("get all cars stubs");
+        return service.findAllWithAvgSalary();
+    }*/
+
+    @GetMapping(value = "/cars/{id}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Car findById(@PathVariable Integer id) {
+        LOGGER.debug("find car by id({})", id);
+        return service.findById(id);
+    }
+
+    @PutMapping()
+    @ResponseStatus(value = HttpStatus.ACCEPTED)
+    public void update(@RequestBody Car car) {
+        LOGGER.debug("update car ({})", car);
+        service.update(car);
+    }
+
+    @DeleteMapping(value = "/cars/{id}")
+    public void delete(@PathVariable("id") int id) {
+        LOGGER.debug("delete car ({})", id);
+        service.delete(id);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Car> add(@RequestBody Car car) {
+
+        LOGGER.debug("add car({})", car);
+        Car result = service.add(car);
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
 }
--- autobaza/rest-app/src/test/java/com/epam/brest/summer/courses2019/rest_app/CarRestControllerTest.java
+++ autobaza/rest-app/src/test/java/com/epam/brest/summer/courses2019/rest_app/CarRestControllerTest.java
@@ -2,6 +2,7 @@ package com.epam.brest.summer.courses2019.rest_app;
 
 import com.epam.brest.summer.courses2019.model.Car;
 import com.epam.brest.summer.courses2019.service.CarService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +12,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,6 +23,12 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Arrays;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = {"classpath:rest-spring-test.xml"})
 public class CarRestControllerTest {
@@ -29,7 +37,9 @@ public class CarRestControllerTest {
     private CarRestController controller;
 
     @Autowired
-    private CarService service;
+    private CarService carService;
+
+    ObjectMapper objectMapper = new ObjectMapper();
 
     private MockMvc mockMvc;
 
@@ -37,24 +47,30 @@ public class CarRestControllerTest {
     public void before() {
         mockMvc = MockMvcBuilders.standaloneSetup(controller)
                 .setMessageConverters(new MappingJackson2HttpMessageConverter())
-                .alwaysDo(MockMvcResultHandlers.print())
+                .alwaysDo(print())
                 .build();
     }
 
-    @AfterEach
+    /*@AfterEach
     public void after() {
         Mockito.verifyNoMoreInteractions(service);
+    }*/
+
+    @AfterEach
+    public void after() {
+        Mockito.reset(carService);
     }
+
 
     @Test
     public void departments() throws Exception {
 
-        Mockito.when(service.findAll()).thenReturn(Arrays.asList(create(0), create(1)));
+        Mockito.when(carService.findAll()).thenReturn(Arrays.asList(createCar(0), createCar(1)));
 
         mockMvc.perform(
                 MockMvcRequestBuilders.get("/cars")
                         .accept(MediaType.APPLICATION_JSON_UTF8)
-        ).andExpect(MockMvcResultMatchers.status().isOk())
+        ).andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].carModel", Matchers.is("def0")))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].carId", Matchers.is(0)))
@@ -62,14 +78,56 @@ public class CarRestControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$[1].carId", Matchers.is(1)))
         ;
 
-        Mockito.verify(service).findAll();
+        Mockito.verify(carService).findAll();
     }
 
-    private Car create(int index) {
+    private Car createCar(int index) {
         Car car = new Car();
         car.setCarModel("def" + index);
         car.setCarId(index);
         return car;
     }
+
+    @Test
+    public void addCar() throws Exception {
+
+        Car expectedCar = createCar(3);
+
+        Car inputCar = new Car();
+        inputCar.setCarModel(expectedCar.getCarModel());
+
+        String json = new ObjectMapper().writeValueAsString(inputCar);
+
+        Mockito.when(carService.add(any(Car.class))).thenReturn(expectedCar);
+
+        MockHttpServletResponse response = mockMvc.perform(
+                post("/department")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+        ).andDo(print())
+                .andExpect(status().isCreated())
+                .andReturn().getResponse();
+
+        String content = response.getContentAsString();
+        Car result = objectMapper.readValue(content, Car.class);
+        assertEquals(expectedCar.getCarModel(), result.getCarModel());
+        assertEquals(expectedCar.getCarId(), result.getCarId());
+
+    }
+
+    @Test
+    public void updateCar() throws Exception {
+
+        Car car = createCar(1);
+        String json = new ObjectMapper().writeValueAsString(car);
+
+        mockMvc.perform(put("/car")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json)
+        ).andExpect(status().isAccepted());
+    }
+
 
 }
